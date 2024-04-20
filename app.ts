4af8df25cc45d7084310ea8feefcfea6e2ba8ad8
AnttoLion <anttolion@gmail.com>
2024-04-19 20:00:39 -0400

    reservation email

--- app.ts
+++ app.ts
@@ -19,27 +19,27 @@ app.use(
 );
 
 
-// Sentry.init({
-//   dsn: "https://159363fd933eaee705ee9f3d4467ab59@o382651.ingest.us.sentry.io/4507070894833664",
+Sentry.init({
+  dsn: "https://159363fd933eaee705ee9f3d4467ab59@o382651.ingest.us.sentry.io/4507070894833664",
 
-//   // Set tracesSampleRate to 1.0 to capture 100%
-//   // of transactions for performance monitoring.
-//   // We recommend adjusting this value in production
-//   tracesSampleRate: 1.0,
-// });
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
-// Sentry.startSpan({
-//   op: "test",
-//   name: "My First Test Transaction",
-// }, () => {
-//   setTimeout(() => {
-//     try {
-//       foo();
-//     } catch (e) {
-//       Sentry.captureException(e);
-//     }
-//   }, 99);
-// });
+Sentry.startSpan({
+  op: "test",
+  name: "My First Test Transaction",
+}, () => {
+  setTimeout(() => {
+    try {
+      foo();
+    } catch (e) {
+      Sentry.captureException(e);
+    }
+  }, 99);
+});
 
 console.log(`Database host is ${process.env.DB_HOST}`);
 console.log(`Database name is ${process.env.DB_NAME}`);
